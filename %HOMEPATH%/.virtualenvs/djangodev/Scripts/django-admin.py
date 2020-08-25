f0f34d4dfca321bf8c871ab7eca9577a6abfd2aa
OANH TRAN <45414817+oanh098@users.noreply.github.com>
2020-08-25 15:38:52 +0700

    Fixed #99999 -- Added a shortcut function to make toast.

---
+++ %HOMEPATH%/.virtualenvs/djangodev/Scripts/django-admin.py
@@ -0,0 +1,7 @@
+#!D:\WORKING\PythonProject\django\%HOMEPATH%\.virtualenvs\djangodev\Scripts\python.exe
+# EASY-INSTALL-DEV-SCRIPT: 'Django==3.2.dev20200821074737','django-admin.py'
+__requires__ = 'Django==3.2.dev20200821074737'
+__import__('pkg_resources').require('Django==3.2.dev20200821074737')
+__file__ = 'D:\\WORKING\\PythonProject\\django\\django\\bin\\django-admin.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
